76fefee6e549c0e07a2fa1ffe5c97deac8db2214
Jagruti Pothal <namitapothal5116@gmail.com>
2025-06-15 08:18:40 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Optional for smooth scroll if not supported natively
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
