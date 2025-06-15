229c9de5a09f3c5f378d459239af236ebaab02dc
Jagruti Pothal <namitapothal5116@gmail.com>
2025-06-15 08:26:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Optional for smooth scroll if not supported natively
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
